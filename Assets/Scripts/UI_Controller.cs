1902ff136d3131ab49e33b561ee0f0448fa7472f
Marcos Elias Cordova <meliascordova@sdsu.edu>
2022-05-01 11:26:38 -0700

    displaying Character Name

--- Assets/Scripts/UI_Controller.cs
+++ Assets/Scripts/UI_Controller.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
+
 public class UI_Controller : MonoBehaviour {
 
-
+    // ---- old code
     public Text CharacterName_Output;
     public InputField CharNameIn;
 
@@ -13,15 +15,34 @@ public class UI_Controller : MonoBehaviour {
     // get reference for the Strength button
     public Text Text_RollDiceStrength_OUTPUT;
 
+    // -- --------------------------------------------------
+
+    public InputField Input_CharacterName;
+    public Text Output_CharacterName;
+
+    public InputField Input_Alignment;
+    public Text Output_Alignment;
+
+
+
+    // ------------- FUNCTIONS --------------------
 
     void Start()
     {
         CharacterName_Output.text = "outputting text";
     }
 
-    public void CallBack_EditEnd()
+
+    // -------------------------------------------------
+
+    public void Display_CharacterName()
     {
-        Debug.Log("CallBack_EditEnd was called");
+        Output_CharacterName.text = Input_CharacterName.text;
+    }
+
+    public void Display_Alignment()
+    {
+        Output_Alignment.text = Input_Alignment.text;
     }
 
     public void DiceRollStrength()
@@ -29,4 +50,6 @@ public class UI_Controller : MonoBehaviour {
         int randomNumber = Random.Range(1, 18);
         Text_RollDiceStrength_OUTPUT.text = randomNumber.ToString();
     }
+
+    // ---------------------------------------------------------------------
 }
